79744f25cc5cbe940355fa2d7e985678491e3906
tanisha <tanisha97533@gmail.com>
2025-03-14 16:04:43 +0530

    second

--- vanshikaka/settings.py
+++ vanshikaka/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!004a3a1p(#17r#)m^#_i+x+&g@8gst%wdb-*jd+lnnzo#@xan
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
